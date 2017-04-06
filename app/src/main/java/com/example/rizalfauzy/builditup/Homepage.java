93b69e09f5f2c9b6a9e52742d62e7e789ec207bc
DESKTOP-FJ0RKJJ\maarif <sahabatarif11@gmail.com>
2017-04-06 22:44:19 +0700

    splash screen & login page

--- app/src/main/java/com/example/rizalfauzy/builditup/Homepage.java
+++ app/src/main/java/com/example/rizalfauzy/builditup/Homepage.java
@@ -1,10 +1,7 @@
 package com.example.rizalfauzy.builditup;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,8 +13,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class Homepage extends AppCompatActivity {
 
@@ -42,8 +37,6 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage2);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -111,7 +104,7 @@ public class Homepage extends AppCompatActivity {
                 View rootView = inflater.inflate(R.layout.fragment_history, container, false);
                 return rootView;
             } else if(getArguments().getInt(ARG_SECTION_NUMBER) == 3){
-                View rootView = inflater.inflate(R.layout.fragment_homepage2, container, false);
+                View rootView = inflater.inflate(R.layout.fragment_account, container, false);
                 return rootView;
             } else {
                 View rootView = inflater.inflate(R.layout.fragment_homepage, container, false);
