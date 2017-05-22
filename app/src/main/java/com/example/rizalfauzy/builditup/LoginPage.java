f9a33e3f02d112b665b29a99ecb1cf85a76fa060
DESKTOP-FJ0RKJJ\maarif <sahabatarif11@gmail.com>
2017-05-22 21:22:53 +0700

    debug login page

--- app/src/main/java/com/example/rizalfauzy/builditup/LoginPage.java
+++ app/src/main/java/com/example/rizalfauzy/builditup/LoginPage.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
@@ -11,10 +12,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.rizalfauzy.builditup.model.user.UserUpdate;
+import com.example.rizalfauzy.builditup.rest.APIClient;
+import com.example.rizalfauzy.builditup.rest.UserAPIInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class LoginPage extends Activity  {
     Button b1,b2,b3;
     EditText ed1,ed2;
+    UserAPIInterface mUserAPIInterface;
 
 
     int counter = 3;
@@ -33,27 +43,38 @@ public class LoginPage extends Activity  {
         b3 = (Button) findViewById(R.id.signup);
 
 
+        mUserAPIInterface = APIClient.getClient().create(UserAPIInterface.class);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ed1.getText().toString().equals("admin") &&
-                        ed2.getText().toString().equals("admin")) {
-                    Toast.makeText(getApplicationContext(),
-                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                Call<UserUpdate> result = mUserAPIInterface.userLogin(ed1.getText().toString(), ed2.getText().toString());
+                result.enqueue(new Callback<UserUpdate>() {
+                    @Override
+                    public void onResponse(Call<UserUpdate> call, Response<UserUpdate> response) {
+                        if (response.body().getStatus().equals("success")) {
+                            Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(LoginPage.this, Homepage.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                            counter--;
 
-                    Intent i = new Intent(LoginPage.this, Homepage.class);
-                    startActivity(i);
-
-                }else{
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-
-
-
-                    counter--;
-                    if (counter == 0) {
-                        b1.setEnabled(false);
+                            if (counter == 0) {
+                                b1.setEnabled(false);
+                                if (counter == 0) {
+                                    b1.setEnabled(false);
+                                }
+                            }
+                        }
                     }
-                }
+
+                    @Override
+                    public void onFailure(Call<UserUpdate> call, Throwable t) {
+                        Log.e("Retrofit Get", t.toString());
+                    }
+
+                });
             }
         });
 
